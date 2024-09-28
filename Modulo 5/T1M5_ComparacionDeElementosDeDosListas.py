0caaaeb0009700f086e9ff09731bff0fed53730f
Enz0fc <enzofc2003@fpuna.edu.py>
2024-09-28 11:13:35 -0400

    28/09/24

--- Modulo 5/T1M5_ComparacionDeElementosDeDosListas.py
+++ Modulo 5/T1M5_ComparacionDeElementosDeDosListas.py
@@ -1,16 +1,20 @@
+
 def mostrar_elementosComunes(A,B):
+    '''Función que muestra los elementos comunes de A y B'''
+    #Implementar de acuerdo a lo solicitado
     for i in A:
         if i in B:
             print(i,end=' ')
-
-def mostrar_elementosEnBNoEnA(A,B):
+def mostrar_elementosEnANoEnB(A,B):
+    '''Función que muestra los elementos de A que no están en B'''
+    #Implementar de acuerdo a lo solicitado
     for i in A:
         if i not in B:
             print(i,end=' ')
-            
+
 def mostrar_elementosEnBNoEnA(A,B):
+    '''Función que muestra los elementos de B que no están en A'''
+    #Implementar de acuerdo a lo solicitado
     for i in B:
         if i not in A:
             print(i,end=' ')
-
-mostrar_elementosComunes([1,2,3],[0,2,3])
